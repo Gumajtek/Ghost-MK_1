186dd88e8363030f15ca05c1ac16d41001e1a8c3
Gumajtek <gumajtek@gmail.com>
2020-07-23 22:17:40 +0200

    Django revomed, way too complicated for a basic webstream. #teamFlask

--- Ghost/src/pages/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
-
-def home_view(*args, **kwargs):
-    return HttpResponse("<h1>Chuje Muje Dżango Srango</h1>")
-    
-def contact_view(*args, **kwargs):
-    return HttpResponse("<h1>  0-700 wypierdalaj</h1>")
